e38ad8164daf162f7fd5da91d2344858086d26c8
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-04 00:13:55 +0200

    Fetch post by ID

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default function(state = {}, action) {
 	switch(action.type) {
+		case FETCH_POST:
+			return { ...state, [action.payload.data.id]: action.payload.data };
 		case FETCH_POSTS:
 			return _.mapKeys(action.payload.data, 'id');
 		default:
