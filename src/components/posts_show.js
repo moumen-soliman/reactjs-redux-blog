67670ccfb74c580b5ed722977bfffa0227672a32
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-04 00:05:24 +0200

    Build posts show component

---
+++ src/components/posts_show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsShow extends Component {
+	render() {
+		return (
+			<div>
+				PostsShow!
+			</div>
+		);
+	}
+}
+
+export default PostsShow;
