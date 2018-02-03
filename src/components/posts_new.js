09e74eeb5e656dbbbac6ad646623a6cb20b32164
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-03 23:50:24 +0200

    Build CreatePost action

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
 	renderField(field) {
@@ -21,7 +23,7 @@ class PostsNew extends Component {
 	}
 
 	onSubmit(values) {
-		console.log(values);
+		this.props.createPost(values);
 	}
 
 	render() {
@@ -72,4 +74,6 @@ function validate(values) {
 export default reduxForm({
 	validate,
 	form: 'PostsNewForm'
-})(PostsNew);
+})(
+	connect(null,{ createPost })(PostsNew)
+);
