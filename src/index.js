d7c6c7f3687b0fa5a6d773e301daa8f43f6f3670
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-03 21:10:09 +0200

    Create fetch posts action & install axios with GET

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  	<BrowserRouter>
+  		<div>
+  			<Route path="/" component={PostsIndex} />
+  		</div>
+  	</BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
